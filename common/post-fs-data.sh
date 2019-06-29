4eb82ecbb12d37e46e37881d4c79115785506941
Jerry <isjerryxiao@outlook.com>
2019-06-29 13:14:48 +0800

    patch sepolicy for scrcpy

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -11,3 +11,11 @@ cd /
 # Change size below
 SIZE=67108864
 mount -t tmpfs -o rw,nosuid,nodev,uid=2000,gid=2000,mode=0771,size=${SIZE} tmpfs /data/local/tmp && echo "Mounted tmpfs on /data/local/tmp"
+# setup sepolicy
+#============= adbd ==============
+magiskpolicy --live "allow adbd adbd_tmpfs file { create open relabelfrom setattr unlink }"
+magiskpolicy --live "allow adbd shell_data_file file relabelto"
+magiskpolicy --live "allow adbd tmpfs dir { add_name remove_name write }"
+#============= shell ==============
+magiskpolicy --live "allow shell adbd_tmpfs file getattr"
+magiskpolicy --live "allow shell tmpfs dir { add_name remove_name write }"
